7e774ac3eb4af6e7c9a1c3d324d78aea7af23424
Amit Kapadia <amit@Amits-MacBook-Pro.local>
2019-05-21 09:58:13 +0100

    Moved CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import './App.scss';
-import 'bootstrap/scss/bootstrap.scss';
+
 
 import Home from './components/home';
 import { Route, Switch} from "react-router-dom";
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-
+import './App.scss';
+import 'bootstrap/scss/bootstrap.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
